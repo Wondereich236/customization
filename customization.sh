3368747fdb81db84a6bfa04ff6843ffe1accfb27
Wondereich236 <kiyotakaa591@gmail.com>
2025-12-02 17:35:37 +0300

    Update customization.sh

--- customization.sh
+++ customization.sh
@@ -9,10 +9,12 @@ xfconf-query -c xfwm4 -p /general/theme -s "Fluent-purple-Dark"
 # İkon teması uygula
 xfconf-query -c xsettings -p /Net/IconThemeName -s "Papirus-Dark"
 
+# Fare teması ve boyutu
+xfconf-query -c xsettings -p /Gtk/CursorThemeName -s "Bibata-Modern-Classic"
+xfconf-query -c xsettings -p /Gtk/CursorThemeSize -s 24
+
 # Duvar kağıdı
 WALL="/usr/share/SatellaOS/backgrounds/anime-girl.jpg"
-
-# Tüm XFCE arka plan alanlarına uygula
 for p in $(xfconf-query -c xfce4-desktop -l | grep last-image); do
     xfconf-query -c xfce4-desktop -p "$p" -s "$WALL"
 done
